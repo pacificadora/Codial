3d63941239d1b20642576156feaf2c4aeb322f78
pacificadora <singhalaman198@gmail.com>
2022-07-19 10:27:08 +0530

    setting up more routes and user controller

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,10 @@ const router = express.Router();
 const homeController = require('../controllers/home_controller')
 
 router.get('/', homeController.home);
+router.use('/users', require('./users'));
+//syntax for adding more routes
+//router.use('/routerName', require('./routerFile'))
+//this way multiple routers can be set up.
 
 console.log("router loaded");
 
