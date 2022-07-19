7dd8f65ab6b3ad7a4fa8cd701fa14b4c03dd6c54
pacificadora <singhalaman198@gmail.com>
2022-07-19 09:16:58 +0530

    creating routes directory

---
+++ routes/index.js
@@ -0,0 +1,7 @@
+//this is the starting point for routes in the app
+const express = require('express');
+const router = express.Router();
+
+console.log("router loaded");
+
+module.exports = router;
