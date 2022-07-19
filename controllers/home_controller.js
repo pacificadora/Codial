b8119dc7ddf28f42068c56919609b3b31a0c2a93
pacificadora <singhalaman198@gmail.com>
2022-07-19 09:44:22 +0530

    setting up controller

---
+++ controllers/home_controller.js
@@ -0,0 +1,7 @@
+//syntax
+//module.exports.actionName = function(req,res){}
+
+
+module.exports.home = function(req, res){
+    return res.end('<h1> Express is up for Codial! </h1>');
+}
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 //this is the starting point for routes in the app
 const express = require('express');
 const router = express.Router();
+const homeController = require('../controllers/home_controller')
+
+router.get('/', homeController.home);
 
 console.log("router loaded");
 
