f07494aa72d7513f07f5098dec7d841f7f9832b2
pacificadora <singhalaman198@gmail.com>
2022-07-22 09:39:05 +0530

    creating controllers and routes for signup and sign in page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,23 @@ module.exports.profile = function(req, res){
         title: 'User Profile',
     })
 }
+
+
+//render the sign up page
+module.exports.signup = function(req, res){
+    return res.render('user_sign_up', {
+        title: 'Codial | Sign Up'
+    })
+}
+
+//render the sign in page
+module.exports.signin = function(req, res){
+    return res.render('user_sign_in', {
+        title: 'Codial | Sign In'
+    })
+}
+
+
+
+
+
--- routes/users.js
+++ routes/users.js
@@ -4,4 +4,7 @@ const router = express.Router();
 const userController = require('../controllers/users_controller');
 router.get('/profile', userController.profile);
 
+router.get('/signup', userController.signup);
+router.get('/signin', userController.signin);
+
 module.exports = router;
