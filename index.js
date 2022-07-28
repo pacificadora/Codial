145809310d3d6cd7a66fb3d6d2fc397197ce6e02
pacificadora <singhalaman198@gmail.com>
2022-07-28 18:06:12 +0530

    installing scss and making necessary changes

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
+const sassMiddleware = require('node-sass-middleware');
+
+app.use(sassMiddleware({
+    src: '/assets/scss',
+    dest: '/assets/css',
+    debug: true,
+    outputStyle: 'extended', //expanded suggestion came
+    prefix: '/css'
+}))
 
 
 
