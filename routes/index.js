1deea2cc3a21de63b2adc0504731aeaaec776e32
pacificadora <singhalaman198@gmail.com>
2022-07-29 16:25:01 +0530

    creating the route for posting a post

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const homeController = require('../controllers/home_controller')
 
 router.get('/', homeController.home);
 router.use('/users', require('./users'));
+router.use('/posts', require('./posts'));
 //syntax for adding more routes
 //router.use('/routerName', require('./routerFile'))
 //this way multiple routers can be set up.
