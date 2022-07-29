1e477831272e460f376c22e0d779f4f58efbb32d
pacificadora <singhalaman198@gmail.com>
2022-07-29 16:26:09 +0530

    create a route for posting a post and apply authentication

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,12 +1,26 @@
 //syntax
 //module.exports.actionName = function(req,res){}
-
+const Post = require('../models/post');
 
 module.exports.home = function(req, res){
     // return res.end('<h1> Express is up for Codial! </h1>');
-    console.log(req.cookies);
-    res.cookie('cookie', 25);
-    return res.render('home', {
-        title: "Home",
-    });
+    // console.log(req.cookies);
+    // res.cookie('cookie', 25);
+
+
+    // Post.find({}, function(err, posts){
+    //    return res.render('home', {
+    //     title: "Codial | Home",
+    //     posts: posts,
+    //    }); 
+    // })
+
+
+    //populate the user for each post
+    Post.find({}).populate('user').exec(function(err, posts){
+        return res.render('home', {
+            title: "Codial | Home",
+            posts: posts,
+        });
+    })
 }
